1aac7e82d86b18026af2a97e1bfa202187edf2ba
rehanahmad1974 <39205868+rehanahmad1974@users.noreply.github.com>
2021-02-15 00:47:45 +0500

    Application is working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,52 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = [];
+// import { AuthGuard } from './auth/auth-guard.service'
+import { HomeComponent } from './home/home.component'
+// import { LoginComponent } from './login/login.component'
+import { ManagerHomeComponent } from './manager/manager-home/manager-home.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+
+const routes: Routes = [
+  // Eagerly Loading Modules
+  // ------------------------------------
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  // { path: 'login', component: LoginComponent },
+  // { path: 'login/:redirectUrl', component: LoginComponent },
+
+  // Lazy Loading Modules
+  // (Note: These modules should not be imported in app.module.ts)
+  // ------------------------------------
+  {
+    path: 'accounting',
+    loadChildren: () =>
+      import('./accounting/accounting.module').then((m) => m.AccountingModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule),
+    // canLoad: [AuthGuard],
+  },
+  {
+    path: 'manager',
+    loadChildren: () => import('./manager/manager.module').then((m) => m.ManagerModule),
+    // canLoad: [AuthGuard],
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
+  },
+  {
+    path: 'inventory',
+    loadChildren: () =>
+      import('./inventory/inventory.module').then((m) => m.InventoryModule),
+  },
+  { path: '**', component: PageNotFoundComponent },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { MatIconRegistry } from '@angular/material/icon'
+import { DomSanitizer } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'AIERP';
+  title = 'AIERP'
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'investment',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/investment.svg')
+    )
+  }
 }
