0c18d7e1c588a1f40ca7e7459ea5b3963dfbbf90
Rehan <rehanahmad1974@yahoo.com>
2021-04-30 17:21:29 +0500

    e2e test file updated

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { browser, logging } from 'protractor'
 
 import { AppPage } from './app.po'
 
-describe('workspace-project App', () => {
+describe('AIERP App', () => {
   let page: AppPage
 
   beforeEach(() => {
@@ -11,7 +11,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo()
-    expect(await page.getTitleText()).toEqual('AIERP app is running!')
+    expect(await page.getTitleText()).toEqual('AIERP')
   })
 
   afterEach(async () => {
